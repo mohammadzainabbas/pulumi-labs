c5c2455396ce6ed5bb3818e9d094fa97174c5c76
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-10 17:37:18 +0100

    🔧 Tuned performance parameters for new requirement

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -31,11 +31,11 @@ vpc = awsx.ec2.Vpc(vpc_name, awsx.ec2.VpcArgs(
     subnet_specs=[
         awsx.ec2.SubnetSpecArgs(
             type=awsx.ec2.SubnetType.PUBLIC,
-            cidr_mask=24,
+            cidr_mask=22,
         ),
         awsx.ec2.SubnetSpecArgs(
             type=awsx.ec2.SubnetType.PRIVATE,
-            cidr_mask=24,
+            cidr_mask=22,
         ),
     ],
     tags={
