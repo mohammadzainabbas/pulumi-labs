1cf0cdd982cea0be9528933ab441783ded5ae8a6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-10 17:42:33 +0100

    👨🏻‍💻 Worked on some improvements

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -32,11 +32,10 @@ vpc = awsx.ec2.Vpc(vpc_name, awsx.ec2.VpcArgs(
     subnet_specs=[
         awsx.ec2.SubnetSpecArgs(
             type=awsx.ec2.SubnetType.PUBLIC,
-            cidr_mask=22,
+            name=f"{vpc_name}-public",
         ),
         awsx.ec2.SubnetSpecArgs(
             type=awsx.ec2.SubnetType.PRIVATE,
-            cidr_mask=22,
         ),
     ],
     tags={
