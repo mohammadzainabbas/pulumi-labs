5123952ee74604adc223ae5424350d5e28d3958b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-10 17:57:24 +0100

    🛡️ Strengthened some requirements

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -27,7 +27,7 @@ azs = aws.get_availability_zones(state="available")
 vpc_name = f"{project_name}-vpc"
 vpc = awsx.ec2.Vpc(vpc_name, awsx.ec2.VpcArgs(
     # enable_dns_support=True,
-    # number_of_availability_zones=len(azs.names),
+    number_of_availability_zones=len(azs.names),
     # instance_tenancy="default",
     subnet_specs=[
         awsx.ec2.SubnetSpecArgs(
