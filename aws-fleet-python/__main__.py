ebf9cee1330e15ca23841da2f48aec2c3524e965
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 13:20:10 +0100

    🌍 Made new features

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -73,6 +73,34 @@ sec_group = aws.ec2.SecurityGroup("secGroup",
         cidr_blocks=["0.0.0.0/0"],
     )])
 
+security_group = aws.ec2.SecurityGroup(
+    "group",
+    vpc_id=vpc.vpc_id,
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=22,
+            to_port=22,
+            protocol="tcp",
+            cidr_blocks=["203.0.113.25/32"],
+        ),
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=443,
+            to_port=443,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    egress=[
+        aws.ec2.SecurityGroupEgressArgs(
+            from_port=0,
+            to_port=0,
+            protocol="-1",
+            cidr_blocks=["0.0.0.0/0"],
+        )
+    ],
+)
+
+
 # # Create and launch an EC2 instance into the public subnet.
 # server = aws.ec2.Instance("server",
 #     instance_type=instance_type,
