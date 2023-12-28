2be8b0087a27c98f4e58247b4fc66a1820f6dd53
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 19:51:45 +0100

    🎉 Celebrated completing some features

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -3,5 +3,5 @@
 cd /home/ubuntu || exit
 sudo apt-get update -y && sudo apt-get install -y python3-pip || echo "Failed to install python3-pip"
 echo "Hello, World from Pulumi!" > index.html
-nohup python3 -m http.server 80 &
+nohup python3 -m http.server 80 > ~/file.txt 2>&1 &
 cd - || exit
