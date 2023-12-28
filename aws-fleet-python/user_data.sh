36bdfd34f6b734624ea106e15e0e75fd804e1a7c
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 18:33:26 +0100

    🚸 Improved an improved code

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "Hello, World from Pulumi!" > index.html
 nohup python3 -m http.server 80 &
-
-aws --region eu-west-3 ec2 allocate-address --domain vpc --query 'AllocationId' --output text
