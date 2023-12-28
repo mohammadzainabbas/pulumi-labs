713cfdbe5b611ca5a9ebff9546fdba8ad15dccd0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 16:38:30 +0100

    📈 Better workflow for some requirements

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -183,6 +183,7 @@ launch_template = aws.ec2.LaunchTemplate(
 # Export the instance's publicly accessible IP address and hostname.
 pulumi.export("ami", ami)
 # pulumi.export("instance_types", instance_types)
+pulumi.export("azs", azs.names)
 pulumi.export("vpc", vpc_name)
 pulumi.export("launch_template", launch_template)
 
