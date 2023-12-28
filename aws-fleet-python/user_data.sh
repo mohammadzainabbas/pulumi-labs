b4f7059b674507445cd0fb0742776310429f40c0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 18:50:17 +0100

    🛠️ Refactored some updates

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# set -ex
+set -ex
 
-# INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-INSTANCE_ID=169.254.169.254
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 AWS_REGION=$1
 ALLOCATION_ID=$2  # replace <YourRegion> with your region
-echo aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"   # replace <YourRegion> with your region
+aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"   # replace <YourRegion> with your region
 
 echo "Hello, World from Pulumi!" > index.html
 echo nohup python3 -m http.server 80 &
