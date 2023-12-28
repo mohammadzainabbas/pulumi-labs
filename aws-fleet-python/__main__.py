eb64a51a9b2d8fe1ab8d6463772cc9a73c223983
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 16:11:23 +0100

    ⛔️ Removed unnecessary code

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -11,7 +11,7 @@ instance_types = config.get("instanceTypes") if config.get("instanceTypes") is n
 vpc_network_cidr = config.get("vpcNetworkCidr") if config.get("vpcNetworkCidr") is not None else "10.0.0.0/16"
 keypair = config.get("keypair") if config.get("keypair") is not None else "jarvis"
 
-valid_until = datetime.now() + timedelta(days=365)
+valid_until = datetime.now() + timedelta(days=365) # 1 year from now
 
 # Look up the latest AWS Deep Learning AMI GPU CUDA i.e: ami-0a8da46354e76997e
 ami = aws.ec2.get_ami(
