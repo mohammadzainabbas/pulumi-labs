1916a5f0be049e7e9ea898d0f48312ae6ca511bd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 18:46:46 +0100

    🕵🏻 Conducted code review for an improvement

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -2,8 +2,9 @@
 set -ex
 
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+AWS_REGION=$1
 ALLOCATION_ID=$2  # replace <YourRegion> with your region
-aws --region $1 ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $ALLOCATION_ID   # replace <YourRegion> with your region
+aws --region $AWS_REGION ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $ALLOCATION_ID   # replace <YourRegion> with your region
 
 echo "Hello, World from Pulumi!" > index.html
 nohup python3 -m http.server 80 &
