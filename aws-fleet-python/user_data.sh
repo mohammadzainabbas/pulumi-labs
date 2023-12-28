cff28a3cb1c4a8ab3ee5066b14eee801db5bc539
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-28 18:52:23 +0100

    👀 Reviewed and merged new code

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -3,7 +3,7 @@ set -ex
 
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 AWS_REGION='<AWS Region>'  # replace with your region
-ALLOCATION_ID='<Allocation-ID>'  # replace with your allocation id
+ALLOCATION_ID='<Elastic IP Allocation-ID>'  # replace with your allocation id
 aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"   # replace <YourRegion> with your region
 
 echo "Hello, World from Pulumi!" > index.html
