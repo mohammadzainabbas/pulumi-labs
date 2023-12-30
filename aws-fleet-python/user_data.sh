5a65090bf6a8d07ba08afcf83b278ea3782c7ad1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-30 16:51:15 +0100

    🚀 Deployed a new feature

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -5,7 +5,4 @@ AWS_REGION='<AWS Region>'  # replace with your region
 ALLOCATION_ID='<Elastic IP Allocation-ID>'  # replace with your allocation id
 aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"   # replace <YourRegion> with your region
 
-echo "Hello, World from Pulumi!" > index.html
-nohup python3 -m http.server 80 &
-
 sudo apt install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Pulumi</h1>' | sudo tee /var/www/html/index.html
