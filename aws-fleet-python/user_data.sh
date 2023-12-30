517ee8c6d5224c03ca665ee7cba08fe6c8d64349
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-30 16:48:08 +0100

    🛡️ Strengthened some code

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-home_dir="/home/ubuntu"
-output_file="$home_dir/output.log"
-docker_vol="$home_dir/docker_volume"
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+AWS_REGION='<AWS Region>'  # replace with your region
+ALLOCATION_ID='<Elastic IP Allocation-ID>'  # replace with your allocation id
+aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"   # replace <YourRegion> with your region
 
-cd $home_dir || exit
-sudo apt-get update -y | tee -a $output_file
-sudo apt-get install -y python3-pip docker.io | tee -a $output_file
 echo "Hello, World from Pulumi!" > index.html
-nohup python3 -m http.server 80 > file.txt 2>&1 &
-mkdir -p $home_dir/docker_volume
-sudo systemctl start docker.service
-docker run --network=host -v $docker_vol:/dev/shm -d --name graphstorm mohammadzainabbas/graphstorm:local | tee -a $output_file
-cd - || exit
+nohup python3 -m http.server 80 &
