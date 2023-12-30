de4431e2bfc7052a6ca6dd885d5b554a9b439a3a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-30 18:12:51 +0100

    🎭 Added support for some modules

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+sudo apt install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Pulumi</h1>' | sudo tee /var/www/html/index.html
+
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 AWS_REGION='<AWS Region>'  # replace with your region
 ALLOCATION_ID='<Elastic IP Allocation-ID>'  # replace with your allocation id
 aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID"   # replace <YourRegion> with your region
-
-sudo apt install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Pulumi</h1>' | sudo tee /var/www/html/index.html
