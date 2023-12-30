a015b4194dac79e4458ca8cd3df9accf6f825016
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-30 16:54:01 +0100

    🔥 Removed unused improvement

--- aws-fleet-python/__main__.py
+++ aws-fleet-python/__main__.py
@@ -97,7 +97,15 @@ security_group = aws.ec2.SecurityGroup(
 )
 
 # Create a static IP address for the instance.
-elastic_ip = aws.ec2.Eip(f"{project_name}-elastic-ip", domain="vpc")
+elastic_ip_name = f"{project_name}-elastic-ip"
+elastic_ip = aws.ec2.Eip(
+    elastic_ip_name,
+    domain="vpc",
+    tags={
+        "Name": elastic_ip_name,
+        "Project": project_name,
+    }
+)
 
 # Define the EBS block device mappings
 block_device_mappings = [
