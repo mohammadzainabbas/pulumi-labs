956de03eb6500e98aab4a67c182b90da2489a915
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-30 14:57:44 +0100

    🔧 Tuned performance parameters for some code

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -12,7 +12,9 @@ run() {
 
 run cd $home_dir || exit
 run sudo apt-get update -y && sudo apt-get install -y python3-pip || echo "Failed to install python3-pip"
+run sudo apt-get install -y docker.io || echo "Failed to install docker.io"
 run echo "Hello, World from Pulumi!" > index.html
 run nohup python3 -m http.server 80 > file.txt 2>&1 &
 run mkdir -p $home_dir/docker_volume
+
 run cd - || exit
