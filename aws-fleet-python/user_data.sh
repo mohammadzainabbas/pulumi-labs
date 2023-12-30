3745793746df37c9d3d1754cf216850e5d9d1881
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-30 15:17:20 +0100

    🚧 Work in progress on a module

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -3,18 +3,12 @@
 home_dir="/home/ubuntu"
 output_file="$home_dir/output.log"
 docker_vol="$home_dir/docker_volume"
-run() {
-    # shellcheck disable=SC2145
-    echo "Running: $@"
-    # shellcheck disable=SC2145
-    "$@ | tee -a $output_file"
-}
 
 cd $home_dir || exit
-sudo apt-get update -y
-sudo apt-get install -y python3-pip docker.io
+sudo apt-get update -y | tee -a $output_file
+sudo apt-get install -y python3-pip docker.io | tee -a $output_file
 echo "Hello, World from Pulumi!" > index.html
-run nohup python3 -m http.server 80 > file.txt 2>&1 &
+nohup python3 -m http.server 80 > file.txt 2>&1 & | tee -a $output_file
 mkdir -p $home_dir/docker_volume
-run docker run --network=host -v $docker_vol:/dev/shm -d --name graphstorm mohammadzainabbas/graphstorm:local || echo "Failed to run docker container"
+run docker run --network=host -v $docker_vol:/dev/shm -d --name graphstorm mohammadzainabbas/graphstorm:local | tee -a $output_file
 cd - || exit
