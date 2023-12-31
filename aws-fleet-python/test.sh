46640df02bc3cb21f30178f0de8b7177e74b1e2b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 07:11:12 +0100

    🛡️ Adding missing validation for security improvements

--- aws-fleet-python/test.sh
+++ aws-fleet-python/test.sh
@@ -49,7 +49,7 @@ _filename="pulumi.svg"
 _pulumi="https://app.pulumi.com/mohammadzainabbas/projects"
 
 success_notify() {
-	_msg="Disk space is low at 5.1 GB"
+	_msg="Instance ID: '$INSTANCE_ID' deployed successfully"
 	_title="New '$INSTANCE_ID' deployed"
 
     curl ntfy.sh \
