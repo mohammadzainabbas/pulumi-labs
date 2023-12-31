a79cd758369a4f32091246acee9cda9df6c4b562
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 06:10:40 +0100

    🎭 Added support for some updates

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -34,7 +34,12 @@ WEB_PAGE="""
 </html>
 """
 
-
+setup_aws_cli() {
+  echo "Setting up AWS CLI..."
+  sudo apt-get install -y python3-pip
+  sudo pip3 install awscli --upgrade
+  aws configure set default.region $REGION
+}
 
 (sudo apt update -y && sudo apt install -y apache2 && sudo systemctl start apache2 && sudo systemctl enable apache2 && echo "$WEB_PAGE" | sudo tee /var/www/html/index.html $output_file) && (curl \
   -H "Title: Docker build has finished successfully \
