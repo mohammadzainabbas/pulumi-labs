cbc2490cf3e4640660a5f565448c6dacb305d220
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 05:03:29 +0100

    🐘 Fixed issues in a module

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -13,4 +13,6 @@ ALLOCATION_ID='<Elastic IP Allocation-ID>'  # replace with your allocation id
 export TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600")
 curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/hostname
 curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region
+pip install awscli
+
 aws --region "$AWS_REGION" ec2 associate-address --instance-id "$INSTANCE_ID" --allocation-id "$ALLOCATION_ID" &>> $output_file  # replace <YourRegion> with your region
