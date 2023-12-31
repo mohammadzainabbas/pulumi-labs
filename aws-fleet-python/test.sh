2245f7f204b11e1cbff99bb4da6f489e81d0a996
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 06:48:42 +0100

    📦 Packaged some updates

--- aws-fleet-python/test.sh
+++ aws-fleet-python/test.sh
@@ -42,6 +42,7 @@ setup_instance() {
 # ------------------------------
 topic="mohammadzainabbas"
 _click="https://github.com/mohammadzainabbas/pulumi-labs"
+_actions="[{ \"action\": \"view\", \"label\": \"Admin panel\", \"url\": \"https://filesrv.lan/admin\" }]"
 success_notify() {
 	_msg="Disk space is low at 5.1 GB"
 	_title="Success: Low disk space alert"
