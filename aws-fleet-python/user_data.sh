48e1c1147059722e8ccae2a50e9ffddd492708ed
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 05:50:22 +0100

    🔒 Enhanced an update

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -29,6 +29,7 @@ WEB_PAGE="""
 <p>Public Hostname: $HOSTNAME</p>
 <p>Region: $REGION</p>
 <p>AMI ID: $AMI_ID</p>
+<p>Made with ♥ using <a href="https://pulumi.com">Pulumi</a>.</p>
 </body>
 </html>
 """
