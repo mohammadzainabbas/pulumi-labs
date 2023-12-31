33620c465135667eab7cb17e56b85930a7995e85
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 08:46:23 +0100

    👨🏻‍💻 Worked on new code

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -68,7 +68,7 @@ start_notify() {
         \"topic\": \"$topic\",
         \"message\": \"$_msg\",
         \"title\": \"$_title\",
-        \"tags\": [\"white_check_mark\",\"computer\",\"tada\"],
+        \"tags\": [\"white_check_mark\",\"package\",\"tada\"],
         \"priority\": 4,
         \"attach\": \"$_attach\",
         \"filename\": \"$_filename\",
