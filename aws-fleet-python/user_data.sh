b9403c156701f68fa7ea115e41855728d0155de2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 08:48:33 +0100

    📈 Better workflow for a new module

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -74,8 +74,7 @@ start_notify() {
         \"click\": \"$_click\",
         \"actions\": [
 				{ \"action\": \"view\", \"label\": \"Open GitHub\", \"url\": \"$_project_link\", \"clear\": false }, 
-				{ \"action\": \"view\", \"label\": \"View Pulumi\", \"url\": \"$_pulumi\", \"clear\": false },
-				{ \"action\": \"view\", \"label\": \"View Website\", \"url\": \"$_web_url\", \"clear\": false }
+				{ \"action\": \"view\", \"label\": \"View Pulumi\", \"url\": \"$_pulumi\", \"clear\": false }
 			]
     }"
 }
