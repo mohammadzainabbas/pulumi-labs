70bbee6e961a1aaf980a557efc1cbd031faa51e2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-12-31 08:51:27 +0100

    🚧 Work in progress on new features

--- aws-fleet-python/user_data.sh
+++ aws-fleet-python/user_data.sh
@@ -78,7 +78,7 @@ start_notify() {
 }
 
 end_notify() {
-	_title="Took '$TIME' to run setup scripts on '$INSTANCE_TYPE' with '$PUBLIC_IP' IPv4 🦋"
+	_title="Took '$TIME' to run setup scripts on '$INSTANCE_TYPE' with '$PUBLIC_IP' IPv4 🌟"
 	_msg="Setup scripts completed on Instance ID: '$INSTANCE_ID' with AMI: '$AMI_ID' at '$AWS_REGION' by account: '$ACCOUNT_ID' 🚧"
 
     curl ntfy.sh \
