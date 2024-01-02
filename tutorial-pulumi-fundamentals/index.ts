c091b21ee9db0a31d03968498a1e4d0022e91559
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-03 00:25:27 +0100

    👀 Reviewed and merged some updates

--- tutorial-pulumi-fundamentals/index.ts
+++ tutorial-pulumi-fundamentals/index.ts
@@ -1,6 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as docker from "@pulumi/docker";
 
+const projectName = pulumi.getProject();
 const stack = pulumi.getStack();
 
 pulumi.log.info(`Hello, world! You are running the ${stack} stack!`);
