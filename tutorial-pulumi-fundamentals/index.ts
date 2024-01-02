564078f9bf28463f14c2358a0178ddd5a223e7a8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-03 00:24:45 +0100

    🔨 Refactored complex functions for simplicity

--- tutorial-pulumi-fundamentals/index.ts
+++ tutorial-pulumi-fundamentals/index.ts
@@ -5,3 +5,8 @@ const stack = pulumi.getStack();
 
 pulumi.log.info(`Hello, world! You are running the ${stack} stack!`);
 
+/* Pull the backend image from Docker Hub */
+const backendImageName = "backend";
+const backend = new docker.RemoteImage(`${backendImageName}Image`, {
+    name: "pulumi/tutorial-pulumi-fundamentals-backend:latest",
+});
