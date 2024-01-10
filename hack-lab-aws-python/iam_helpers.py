59c5be311a916f6f4bba219cbfa1352d7f05975a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-10 17:23:54 +0100

    🚧 Work in progress on some modules

--- hack-lab-aws-python/iam_helpers.py
+++ hack-lab-aws-python/iam_helpers.py
@@ -1,11 +1,7 @@
-# Copyright 2018-2019, James Nugent.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public License,
-# v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain
-# one at http://mozilla.org/MPL/2.0/.
-
 """
 Contains helper methods for building IAM policies.
+
+Link: https://github.com/jen20/pulumi-aws-vpc/blob/master/python/jen20_pulumi_aws_vpc/iam_helpers.py
 """
 import json
 
