56d9220954db860e60587fa515256b876a98e87a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-10 14:20:49 +0100

    🛡️ Strengthened a feature

--- hack-lab-aws-python/__main__.py
+++ hack-lab-aws-python/__main__.py
@@ -7,7 +7,7 @@ from json import loads
 # Get some configuration values or set default values.
 dir_name = pulumi.get_project()
 aws_region = aws.get_region().name
-project_name = "aws-graphstorm"
+project_name = "hack-lab"
 config = pulumi.Config()
 instance_types = config.get("instanceTypes") if config.get("instanceTypes") is not None else ['t3.micro', 't4g.small']
 vpc_network_cidr = config.get("vpcNetworkCidr") if config.get("vpcNetworkCidr") is not None else "10.0.0.0/16"
