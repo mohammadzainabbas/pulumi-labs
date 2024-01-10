08a99e3e58e9bc76974ab55826370dee941325c1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-10 20:44:21 +0100

    🧱 Laid groundwork for some requirements

--- hack-lab-aws-python/scripts/setup_desktop.sh
+++ hack-lab-aws-python/scripts/setup_desktop.sh
@@ -38,8 +38,7 @@ setup_instance() {
     sudo apt-get update -y && sudo apt full-upgrade -y
     sudo apt install -y wget kali-linux-headless kali-desktop-xfce xorg xrdp
     sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
-    
-    
+    sudo systemctl enable xrdp --now
 }
 
 # ------------------------------
