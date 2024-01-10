4cf4847f88bffa9f87a4db6f64566dad8af303d6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-10 18:05:01 +0100

    💾 Saved changes for a new feature

--- hack-lab-aws-python/vpc.py
+++ hack-lab-aws-python/vpc.py
@@ -323,7 +323,7 @@ class VpcxArgs:
             base_tags: Optional[pulumi.Input[Mapping[str, pulumi.Input[str]]]] = {},
             network_cidr: str | None = "10.0.0.0/16",
             availability_zone_names: Optional[pulumi.Input[Sequence[pulumi.Input[str]]]] = aws.get_availability_zones(state="available"),
-            aws_region: pulumi.Input[str] = aws.get_region().name
+            aws_region: pulumi.Input[str] = aws.get_region().name,
         ):
         """
         Constructs a VpcxArgs.
