dad244f1f1cfa8aed5491ed1cfc2d3626fa9f0ef
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-10 20:45:03 +0100

    🎭 Added support for a new feature

--- hack-lab-aws-python/scripts/setup_desktop.sh
+++ hack-lab-aws-python/scripts/setup_desktop.sh
@@ -35,7 +35,7 @@ run() {
 # ------------------------------
 
 setup_instance() {
-    sudo apt-get update -y && sudo apt full-upgrade -y
+    sudo apt-get update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
     sudo apt install -y wget kali-linux-headless kali-desktop-xfce xorg xrdp
     sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
     sudo systemctl enable xrdp --now
