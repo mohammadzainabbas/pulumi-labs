9213ea87732888c17bdeeafa5d150113dd71d878
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-10 18:14:52 +0100

    🕵🏻 Conducted code review for some requirements

--- hack-lab-aws-python/vpc.py
+++ hack-lab-aws-python/vpc.py
@@ -362,17 +362,12 @@ class Vpcx(pulumi.ComponentResource):
     zones = get_availability_zones(state="available")
 
     vpc = Vpcx("example-vpc", VpcxArgs(
-        description="Example VPC",
-        base_tags={
+        vpc_cidr_block="192.168.0.0/16",
+        azs=zones.names,
+        tags={
             "Project": "Python Example VPC",
         },
-        base_cidr="192.168.0.0/16",
-        availability_zone_names=zones.names,
-        zone_name="example.local",
-        create_s3_endpoint=True,
-        create_dynamodb_endpoint=True,
     ))
-    vpc.enableFlowLoggingToCloudWatchLogs("ALL")
 
     export("vpcId", vpc.vpc.id)
     export("publicSubnetIds", [subnet.id for subnet in vpc.public_subnets])
