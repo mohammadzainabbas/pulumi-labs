d3911bc66344f3af548e164fe4529071b9141237
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 23:34:31 +0100

    🛠️ Refactored a module

--- hack-lab-aws-python/utils.py
+++ hack-lab-aws-python/utils.py
@@ -113,7 +113,6 @@ class DownloadUnzipProvider(pulumi.dynamic.ResourceProvider):
 
     def create(self, inputs: DownloadUnzipInputArgs | dict) -> pulumi.dynamic.CreateResult:
         url, output_dir, filename = inputs["url"], inputs["output_dir"], inputs["filename"]
-        output_dir = os.path.expanduser(output_dir)
         if not filename: filename = os.path.basename(url)
         _outs = DownloadUnzipOutputArgs.from_dict(inputs)
         _outs.name = self.name
