909a17854aec63b7f709721e82e55b5061682fce
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 18:41:51 +0100

    👨🏻‍💻 Worked on new requirement

--- hack-lab-aws-python/utils.py
+++ hack-lab-aws-python/utils.py
@@ -18,6 +18,26 @@ import zipfile
 from typing import Any, Callable, Dict, IO, Iterable, Iterator, List, Optional, Tuple, TypeVar, Union
 from urllib.parse import urlparse
 
+def _save_response_content(
+    content: Iterator[bytes],
+    destination: str,
+    length: Optional[int] = None,
+) -> None:
+    with open(destination, "wb") as fh, tqdm(total=length) as pbar:
+        for chunk in content:
+            # filter out keep-alive new chunks
+            if not chunk:
+                continue
+
+            fh.write(chunk)
+            pbar.update(len(chunk))
+
+
+def _urlretrieve(url: str, filename: str, chunk_size: int = 1024 * 32) -> None:
+    with urllib.request.urlopen(urllib.request.Request(url, headers={"User-Agent": USER_AGENT})) as response:
+        _save_response_content(iter(lambda: response.read(chunk_size), b""), filename, length=response.length)
+
+
 def download_url(url, root, filename=None):
     """Download a file from a url and place it in root.
     Args:
