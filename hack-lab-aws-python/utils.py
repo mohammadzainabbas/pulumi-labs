d71f97a116e226f8c747ae11feec1378180f27f8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 20:53:12 +0100

    🐘 Fixed issues in an improvement

--- hack-lab-aws-python/utils.py
+++ hack-lab-aws-python/utils.py
@@ -110,7 +110,7 @@ class DownloadUnzipProvider(pulumi.dynamic.ResourceProvider):
                 else: # remove everything in the temp directory
                     for f in os.listdir(temp_dir): os.remove(os.path.join(temp_dir, f)) 
                 zip_ref.extractall(temp_dir)
-                for filename in os.listdir(output_dir):
+                for filename in os.listdir(temp_dir):
                     if filename.endswith('.ova'):
                         ova_file_path = f"{output_dir}/{filename}"
                         break
