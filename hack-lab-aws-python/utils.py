2432e9a602361fe4d76b99f23e75c371fe119ca4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 20:38:40 +0100

    🎭 Added support for an improvement

--- hack-lab-aws-python/utils.py
+++ hack-lab-aws-python/utils.py
@@ -114,7 +114,7 @@ class DownloadUnzipProvider(pulumi.dynamic.ResourceProvider):
                     "id",
                     keepers={ "file": ova_file_path },
                     byte_length=4
-                )
+                ).hex
                 return pulumi.dynamic.CreateResult(id=_id, outs=_outs)
         except Exception as e:
             raise Exception(f"Failed to download and unzip: {str(e)}")
