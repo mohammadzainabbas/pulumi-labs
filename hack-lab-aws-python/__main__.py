a2d221dc9340014dd3af306951a042680a5b2c07
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 19:31:38 +0100

    🌍 Made an update

--- hack-lab-aws-python/__main__.py
+++ hack-lab-aws-python/__main__.py
@@ -90,7 +90,7 @@ public_access_block = aws.s3.BucketPublicAccessBlock(
 vuln_os_file = f"breach.zip"
 vuln_os_output_dir = f"~/Desktop/vuln"
 vuln_os_url = f"https://download.vulnhub.com/breach/Breach-1.0.zip"
-vuln_os_name = f"breach"
+vuln_os_name = f"{project_name}-breach-vm"
 vuln_os = DownloadUnzip(
     vuln_os_name,
     url=vuln_os_url,
