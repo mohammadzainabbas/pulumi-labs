bbeb9b88ce42994845c5bee0387351e88f9f8d16
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 21:52:46 +0100

    👀 Reviewed and merged some features

--- hack-lab-aws-python/utils.py
+++ hack-lab-aws-python/utils.py
@@ -143,6 +143,9 @@ class DownloadUnzipProvider(pulumi.dynamic.ResourceProvider):
     def delete(self, id: str, _props: DownloadUnzipOutputArgs) -> None:
          
         if os.path.exists(_props.extract_dir):
+            print(f"Deleting {_props.extract_dir}({id}) from {_props.extract_dir}...")
+            os.remove(f"{_props.output_dir}/{_props.filename}")
+        if os.path.exists(_props.extract_dir):
             print(f"Deleting {_props.filename}({id}) from {_props.extract_dir}...")
             os.remove(f"{_props.output_dir}/{_props.filename}")
 
