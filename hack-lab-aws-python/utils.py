74a433b2675688d6d83860a3613997def8c10190
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-11 19:02:38 +0100

    📲 Enhanced some improvements

--- hack-lab-aws-python/utils.py
+++ hack-lab-aws-python/utils.py
@@ -3,6 +3,7 @@ import pathlib
 import urllib.error
 import urllib.request
 from typing import Iterator, Optional, Union
+import pulumi
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
