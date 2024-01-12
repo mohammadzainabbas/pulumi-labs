0fdb76c297fd5acdaf796cadb3460c0df86b85a9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-01-12 08:24:10 +0100

    📲 Enhanced new code

--- hack-lab-aws-python/src/download_zip.py
+++ hack-lab-aws-python/src/download_zip.py
@@ -1,3 +1,16 @@
 import pulumi
 import pulumi_command as command
 import pulumi_random as random
+
+class DownloadZipArgs:
+    def __init__(self,
+                 bucket_name: pulumi.Input[str],
+                 vuln_os_file: pulumi.Input[str],
+                 vuln_os_output_dir: pulumi.Input[str],
+                 vuln_os_url: pulumi.Input[str],
+                 vuln_os_name: pulumi.Input[str]):
+        self.bucket_name = bucket_name
+        self.vuln_os_file = vuln_os_file
+        self.vuln_os_output_dir = vuln_os_output_dir
+        self.vuln_os_url = vuln_os_url
+        self.vuln_os_name = vuln_os_name
