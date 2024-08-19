0a9583c12792a6e429356ea482bcab21cb856653
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-08-19 16:14:51 +0200

    🐘 Fixed issues in a module

---
+++ mqtt-aws-python/__main__.py
@@ -0,0 +1,75 @@
+import pulumi
+import pulumi_aws as aws
+
+# Create a new VPC
+vpc = aws.ec2.Vpc("newVpc",
+    cidr_block="10.0.0.0/16",
+    enable_dns_support=True,
+    enable_dns_hostnames=True,
+    tags={
+        "Name": "newVpc",
+    }
+)
+
+# Create a public subnet
+subnet = aws.ec2.Subnet("publicSubnet",
+    vpc_id=vpc.id,
+    cidr_block="10.0.1.0/24",
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": "publicSubnet",
+    }
+)
+
+# Create an internet gateway
+internet_gateway = aws.ec2.InternetGateway("internetGateway",
+    vpc_id=vpc.id,
+    tags={
+        "Name": "internetGateway",
+    }
+)
+
+# Create a route table
+route_table = aws.ec2.RouteTable("routeTable",
+    vpc_id=vpc.id,
+    routes=[
+        aws.ec2.RouteTableRouteArgs(
+            cidr_block="0.0.0.0/0",
+            gateway_id=internet_gateway.id,
+        ),
+    ],
+    tags={
+        "Name": "routeTable",
+    }
+)
+
+# Associate route table with subnet
+route_table_association = aws.ec2.RouteTableAssociation("routeTableAssociation",
+    subnet_id=subnet.id,
+    route_table_id=route_table.id
+)
+
+# Define an Amazon MQ Broker
+broker = aws.mq.Broker("simpleActiveMQBroker",
+    broker_name="simple-activemq-broker",
+    engine_type="ActiveMQ",
+    engine_version="5.15.14",
+    host_instance_type="mq.t3.micro",
+    security_groups=[],  # Add security group IDs if needed
+    subnet_ids=[subnet.id],  # Subnet where the broker should be deployed
+    publicly_accessible=False,
+    users=[aws.mq.BrokerUserArgs(
+        username="admin",
+        password="ChangeMe123!",  # Change to a secure password
+    )],
+    logs=aws.mq.BrokerLogsArgs(
+        general=True,
+    ),
+    tags={
+        "environment": "dev"
+    }
+)
+
+pulumi.export("broker_id", broker.id)
+pulumi.export("broker_arn", broker.arn)
+pulumi.export("broker_url", broker.instances[0].endpoints[0])
