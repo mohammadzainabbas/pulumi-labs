bfd2826a90212f90e78c3b50d27d78c475a446e1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-08-19 17:35:04 +0200

    🐘 Fixed issues in an improved code

--- mqtt-aws-python/__main__.py
+++ mqtt-aws-python/__main__.py
@@ -196,7 +196,8 @@ thing = aws.iot.Thing(
 )
 
 
-["iot:Connect", "iot:Publish", "iot:Subscribe", "iot:Receive", "iot:UpdateThingShadow", "iot:GetThingShadow", "iot:DeleteThingShadow"]
+
+policies = ["iot:Connect", "iot:Publish", "iot:Subscribe", "iot:Receive", "iot:UpdateThingShadow", "iot:GetThingShadow", "iot:DeleteThingShadow"]
 
 # Create a policy
 policy = aws.iot.Policy(
